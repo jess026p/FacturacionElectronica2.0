8889dfccd91c490e28c0b15201bb681ca9dea00c
Esthefania Villacres <svillacres6104@uta.edu.ec>
2023-07-17 23:16:54 -0500

    CU-865cf0q0n - Implementar los servicios del módulo de  clientes

---
+++ servicios/serviciosFacturacion/app/Models/Vendedor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vendedor extends Model
+{
+    use HasFactory;
+    protected $table = 'vendedores';
+     public $timestamps = false;
+    protected $fillable = [
+        'Nombre',
+        'Apellido',
+        'Usuario',
+        'Contraseña',
+        'CorreoElectronico',
+    ];
+}
