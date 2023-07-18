1659badb62fdb0ed683c6f4c846965d4d21e6a37
jess026p <jesifv1999@gmail.com>
2023-07-17 22:41:52 -0500

    CU-865cf6j8m - Crear los componentes de la interfaz del modulo de las ventas

---
+++ front/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './components/login/login.component';
+import { SharedModule } from './components/shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+   
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
